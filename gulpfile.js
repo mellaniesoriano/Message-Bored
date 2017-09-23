c9e91df2198c247b0344fbaf74953fe4427399a0
mellaniesoriano <msoriano808@gmail.com>
2017-09-23 11:37:48 -1000

    initial styles commit

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+var gulp = require('gulp');
+var scss = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+
+browserSync.init({
+  proxy: "localhost:8080"
+});
+
+gulp.task('scss', function () {
+  return gulp.src('./scss/*.scss')
+    .pipe(scss())
+    .pipe(gulp.dest('./public/css'));
+});
+
+gulp.task('watch', function (){
+  gulp.watch('./scss/**/*', ['scss']);
+  gulp.watch('./public/**/*').on('change', browserSync.reload);
+});
+
+gulp.task('default', ['watch']);
